092c13b81f8054a74bf6c5771a546c37c64e7015
Jaechan Hwang <emperor0827@gmail.com>
2024-12-02 20:34:03 +0900

    Update ready-for_exam2

--- ready_for_exam/prob3.c
+++ ready_for_exam/prob3.c
@@ -11,40 +11,64 @@ struct node
 struct node* new_node(int value)
 {
     // make new node
-    
+    struct node* nnode = (struct node*)malloc(sizeof(struct node));
     // save the value
+    nnode->val = value;
 
     // return new node
+    return nnode; 
 }
 
 int size(struct node* head)
 {
+    struct node* tmp = head;
+    int cnt = 0;
+    while(tmp!=NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
     // return size of linkedlist 
-    
 }
 
 int empty(struct node* head)
 {
+    if(head==NULL)return 1;
+    else return 0;
     // if list empty return 1 else return 0
-    
 }
 
-void insert(struct node* head, struct node* new_node)
+void insert(struct node** head, struct node* new_node)
 {
     // insert new_node to end of linkedlist
-    // if there is tail
-    
-
-    // if there is no tail
-    
+    if(head == NULL)
+    {
+        *head = new_node;
+    }
+    else
+    {
+        struct node* tmp = *head;
+        while(tmp->next!=NULL)
+        {
+            tmp = tmp->next;
+        } 
+        tmp->next = new_node;
+    }
 }
 
-int delete(struct node* head, int value)
+int delete(struct node** head, int value)
 {
     // delete node that have value we want
     // if delete successfully, than return 0
     // else return -1
-    
+    if(empty(head))return -1;
+    if((*head)->val == value)
+    {
+        *head = (*head)->next;
+        return 0;
+    }
+
 }
 
 void print_list (struct node* head)
@@ -58,10 +82,10 @@ int main()
     int i, n = 5;
     for(i=0; i<n; i++)
     {
-        insert(head,i*10);
+        insert(&head,new_node(i*10));
     }
     print_list(head);
 
-    delete(head,10);
+    delete(&head,10);
     print_list(head);
 }
